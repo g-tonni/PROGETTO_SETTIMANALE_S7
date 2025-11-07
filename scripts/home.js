6823171c5c345e0e61bc60e30a8bcffb857a9b39
Giada <tonni.giada.210802@gmail.com>
2025-11-07 09:52:01 +0100

    FILE INSERITI

---
+++ scripts/home.js
@@ -0,0 +1,3 @@
+/* headers: {
+"Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2OTBkYTk2MmY0YmQ0NzAwMTU4NWIxZDYiLCJpYXQiOjE3NjI1MDMwMTAsImV4cCI6MTc2MzcxMjYxMH0.prkw1wDA1sxtXmDjhPczT415z6zjAuZsvUDuw3Rj2QY"
+} */
